7c45989a161d8d11177cb879ff83144d20228f0e
lizz <363664470@qq.com>
2019-11-30 13:39:07 +0800

    ha
    Signed-off-by: lizz <363664470@qq.com>

--- ssh.bat
+++ ssh.bat
@@ -1,26 +1,5 @@
 @echo off
 
-rem  Update: 2019-03-11
-rem  Author: Liu Yue (hangxingliu@gmail.com)
-rem
-rem  Description:
-rem    Pipe ssh invoking from Windows to ssh in WSL.
-rem    And escape character "\\" to "\\\\"
-rem
-rem
-rem  Knowledges:
-rem   1. How to implements string replace in Windows batch:
-rem        set "VAR=%VAR:REPLACE_FROM=REPLACE_TO%"
-rem        Chinese blog: https://www.jb51.net/article/110243.htm
-rem   2. Why prepend  `setlocal enabledelayedexpansion`
-rem        Stackoverflow: https://stackoverflow.com/questions/6679907
-rem   3. How to detect a variable is empty:
-rem        Stackoverflow: https://stackoverflow.com/a/2541820/3831547
-rem   4. Get command exit code: (Just like "$?" on linux)
-rem        Stackoverflow: https://stackoverflow.com/a/334890/3831547
-rem
-rem
-
 setlocal enabledelayedexpansion
 
 set "currentdir=%cd:\=\\%"
